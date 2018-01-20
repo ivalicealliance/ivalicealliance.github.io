a40070d5a09316891e3c4eaa2aad81b1d7e8c26d
Balthier <34898262+balthier-ex@users.noreply.github.com>
2018-01-19 22:17:40 -0800

    Format gulpfile.js
    - Use code formatter on `gulpfile.js`
    - Remove comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
 
 var sassPaths = [
   'bower_components/normalize.scss/sass',
@@ -7,11 +7,11 @@ var sassPaths = [
   'bower_components/motion-ui/src'
 ];
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src('scss/app.scss')
     .pipe($.sass({
       includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
+      outputStyle: 'compressed'
     })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
@@ -20,6 +20,6 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('default', ['sass'], function () {
   gulp.watch(['scss/**/*.scss'], ['sass']);
 });
